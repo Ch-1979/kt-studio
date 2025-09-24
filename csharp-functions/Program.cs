35d1ade1e7a1da4151b0208da2aef291e1a9b9c3
ch-1979 <r.chava@accenture.com>
2025-09-24 17:12:28 +0530

    Fix build: remove Microsoft.Extensions.Azure usage; direct BlobServiceClient registration

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,21 +1,17 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Azure;
+using Azure.Storage.Blobs;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices((ctx, services) =>
     {
-        // Minimal services: BlobServiceClient via connection string
-        services.AddAzureClients(builder =>
+        var config = ctx.Configuration;
+        var storageConn = config["AzureWebJobsStorage"] ?? config["Storage:ConnectionString"];
+        if (!string.IsNullOrWhiteSpace(storageConn))
         {
-            var config = ctx.Configuration;
-            var storageConn = config["AzureWebJobsStorage"] ?? config["Storage:ConnectionString"];
-            if (!string.IsNullOrWhiteSpace(storageConn))
-            {
-                builder.AddBlobServiceClient(storageConn);
-            }
-        });
+            services.AddSingleton(new BlobServiceClient(storageConn));
+        }
     })
     .Build();
 
